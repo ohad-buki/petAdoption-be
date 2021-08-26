b1ac8c69d94d0ba2159cba7b616351f28ed5ae9c
ohad-buki <83958473+ohad-buki@users.noreply.github.com>
2021-08-26 11:34:42 +0300

    try to fix

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const { postgrator } = require("./data/mysqlDB");
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  try {
+    res.send("hello world");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.use("/users", require("./routes/usersRoute.js"));
 app.use("/pets", require("./routes/petsRoute.js"));
 app.use("/likes", require("./routes/likesRoute.js"));
