f8840c89938582dcb88ab181e74a40b2da88c7cb
ohad-buki <83958473+ohad-buki@users.noreply.github.com>
2021-08-26 12:13:19 +0300

    try to fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
-const { postgrator } = require("./data/mysqlDB");
+// const { postgrator } = require("./data/mysqlDB");
 
 app.use(express.json());
 app.use(cors());
@@ -19,12 +19,12 @@ app.use("/users", require("./routes/usersRoute.js"));
 app.use("/pets", require("./routes/petsRoute.js"));
 app.use("/likes", require("./routes/likesRoute.js"));
 
-postgrator
-  .migrate()
-  .then((result) => {
-    console.log("migrate", result);
-    app.listen(port, () => {
-      console.log("server is up");
-    });
-  })
-  .catch((e) => console.log(e));
+// postgrator
+//   .migrate()
+//   .then((result) => {
+//     console.log("migrate", result);
+app.listen(port, () => {
+  console.log("server is up");
+});
+// })
+// .catch((e) => console.log(e));
