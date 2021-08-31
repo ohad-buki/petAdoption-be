9e9119123a9809694ef5a5d03e58b4688ab1f068
ohad-buki <83958473+ohad-buki@users.noreply.github.com>
2021-08-31 17:37:23 +0300

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
-const { postgrator } = require("./data/mysqlDB");
+const { postgrator } = require("./data/mysqldb");
 
 app.use(express.json());
 app.use(cors());
