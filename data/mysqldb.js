5c4d33fcad2f303f2f06c0b58498cc67ec78c765
ohad-buki <83958473+ohad-buki@users.noreply.github.com>
2021-09-01 13:34:04 +0300

    Update mysqldb.js

--- data/mysqldb.js
+++ data/mysqldb.js
@@ -7,17 +7,17 @@ const postgrator = new Postgrator({
   driver: "mysql",
   host: process.env.DB_HOST_DEPLOY,
   port: 3306,
-  database: process.env.DB_USER_DEPLOY,
-  username: process.env.DB_USER_DEPLOY,
-  password: process.env.DB_PASS_DEPLOY,
+  database: process.env.DB_USER,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
   schemaTable: "migrations",
 });
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST_DEPLOY,
-  user: process.env.DB_USER_DEPLOY,
-  password: process.env.DB_PASS_DEPLOY,
-  database: process.env.DB_USER_DEPLOY,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_USER,
 });
 
 exports.pool = pool;
